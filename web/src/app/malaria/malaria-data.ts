bd44a47bf1422473dc929f9100879149f0d9354a
Luis Delgado Romera <luis@solidlines.io>
2025-03-25 09:26:37 +0100

    Add MalariaData class to define structure for malaria-related symptoms

--- web/src/app/malaria/malaria-data.ts
+++ web/src/app/malaria/malaria-data.ts
@@ -1,1 +1,13 @@
-
+export class MalariaData {
+    name!: string;
+    age!: number;
+    fever!: boolean;
+    chills!: boolean;
+    sweating!: boolean;
+    headache!: boolean;
+    nausea!: boolean;
+    vomiting!: boolean;
+    musclePain!: boolean;
+    fatigue!: boolean;
+    otherSymptoms!: string|null;
+}
