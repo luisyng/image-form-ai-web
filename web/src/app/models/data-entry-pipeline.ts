86fe2a011ccc8ef6bbfd5502034bc2c660b48d1e
Luis Delgado Romera <luis@solidlines.io>
2025-04-08 08:33:09 +0200

    Fix processing stage

--- web/src/app/models/data-entry-pipeline.ts
+++ web/src/app/models/data-entry-pipeline.ts
@@ -3,13 +3,16 @@ import { DataEntryConfig } from './data-entry-config';
 import { StageStatus } from '../data-entry-stage/data-entry-stage.component';
 
 export class DataEntryPipeline {
+  c!: DataEntryConfig;
   file: File | null = null;
   extractedText: string | null = null;
   reviewedText: string | null = null;
   formData: MalariaData | null = null;
+  reviewedFormData: MalariaData | null = null;
   backendSubmitStatus: StageStatus = 'normal';
 
   constructor(c: DataEntryConfig) {
+    this.c = c;
     if (c.selectedInputType?.id === 'manual') {
       this.formData = new MalariaData();
     } else {
@@ -23,7 +26,7 @@ export class DataEntryPipeline {
     console.log('File loaded:', file);
   }
   
-  handleTextExtracted(text: string): void {
+  private handleTextExtracted(text: string): void {
     this.extractedText = text;
     this.reviewedText = text;
     console.log('Extracted text:', text);
@@ -39,8 +42,18 @@ export class DataEntryPipeline {
     console.log('Parsed malaria data:', data);
   }
 
+  handleInputProcessed(data: any) {
+    const m = this.c.selectedProcessMethod!;
+    console.log('Input processed:', data);
+    if (m.outputType === 'JSON') {
+      this.handleParsedData(data);
+    } else {
+      this.handleTextExtracted(data);
+    }
+  }
+
   handleFormSubmission(data: any) {
-    this.formData = data;
+    this.reviewedFormData = data;
     console.log('Form submitted:', data);
   }
 
